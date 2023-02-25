bf49ed27135f968f6e337ce4ad3bb91ebc35d2ff
Daniel Salgado <danielsalgado.rom@gmail.com>
2023-02-24 21:08:03 -0600

    [Fixes] [Try 3] Bug fix in Stopwatch, is not working in mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,58 @@
 import './App.css'
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import Stopwatch from './components/stopwatch/Stopwatch'
 import FormAlarm from './components/formAlarm/FormAlarm'
 
 function App() {
+
+  //Constants and Variables
+
   const [time, setTime] = useState( { ms: 0, sec: 0, min: 0, hrs: 0 } )
   const [running, setRunning] = useState(false)
   const [alarm, setAlarm] = useState(null)
   const [message, setMessage] = useState('')
 
   let intervalRef = useRef();
+  let ms = time.ms, sec = time.sec, min = time.min, hrs = time.hrs;
 
-  useEffect(() => {
+  //Functions
 
-    if (running) {
-      intervalRef.current = setInterval(() => {
-        setTime((prevTime) => {
-        let ms= prevTime.ms + 1;
-        let sec = prevTime.sec;
-        let min = prevTime.min;
-        let hrs = prevTime.hrs;
-
-        if (ms === 100) {
-          ms = 0 ;
-          sec = prevTime.sec + 1;
-        }
-        if (sec === 60) {
-          sec = 0 ;
-          min = prevTime.min + 1;
-        }
-        if (min === 60) {
-          min = 0 ;
-          hrs = prevTime.hrs + 1;
-        }
-        return { ms, sec, min, hrs}
-      });
-      }, 10);
-
-    } else if (!running) {
-      clearInterval(intervalRef.current)
+  const run = () => {
+    if(min === 60){
+      hrs++;
+      min = 0;
     }
+    if(sec === 60){
+      min++;
+      sec = 0;
+    }
+    if(ms === 100){
+      sec++;
+      ms = 0;
+    }
+    ms++;
+    return setTime({ms:ms, sec:sec, min:min, hrs:hrs});
+  };
+
+  const start = () => {
+    run();
+    setRunning(true);
+    intervalRef.current = (setInterval(run, 10));
 
     if (alarm !== null) {
       setTimeout(() => {
         alert(message)
         setMessage('')
-      }, alarm + 7);  //We add 7 milliseconds for the variation of the calculations
+      }, alarm - 5);
     }
+  };
 
-    return () => clearInterval(intervalRef.current)
+  const stop = () => {
+
+    setRunning(false);
+    clearInterval(intervalRef.current)
     
-  }, [running])
+  };
 
   function handleReset() {
     setTime({ ms: 0, sec: 0, min: 0, hrs: 0 })
@@ -66,16 +68,17 @@ function App() {
       
       <div className='container__layout'>
         <div className='container__stopwatch'>
+
           <Stopwatch time={time} />
           
           <div className='container__buttons'>
               {running ? (
-                <button onClick={() => {setRunning(false)}} className='button__stopwatch'><i className="fa-solid fa-stop"></i></button>
+                <button onClick={stop} className='button__stopwatch'><i className="fa-solid fa-stop"></i></button>
               ) : (
-                <button onClick={() => {setRunning(true)}} className='button__stopwatch'><i className="fa-solid fa-play"></i></button>
+                <button onClick={start} className='button__stopwatch'><i className="fa-solid fa-play"></i></button>
               )} 
 
-                <button onClick={() => {handleReset()}} className='button__stopwatch'><i className="fa-solid fa-rotate-right"></i></button>
+                <button onClick={() => handleReset()} className='button__stopwatch'><i className="fa-solid fa-rotate-right"></i></button>
           </div>
         </div>
 
